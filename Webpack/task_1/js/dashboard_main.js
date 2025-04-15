810d52f63352333cc91f3c6a78644e808b9e9f33
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-04-15 12:59:26 +0200

    refresh task 1

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -3,14 +3,14 @@ import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
+$('body').append('<button id="click-me">Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
-const updateCounter = () => {
-  count++;
+function updateCounter() {
+  count += 1;
   $('#count').text(`${count} clicks on the button`);
-};
+}
 
-$('button').on('click', _.debounce(updateCounter, 500));
+$('#click-me').on('click', _.debounce(updateCounter, 500));
