ac7cc12ced0982757b31a5ca00db9dbd251c00d9
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-04-27 20:28:17 +0200

    changement export??

--- react_intro/task_1/dashboard/src/Notifications.jsx
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import "./Notifications.css";
+import './Notifications.css';
+import React from 'react';
 
-function Notifications() {
+export default function Notifications() {
   return (
-    <div className="notifications">
-      <p>Here is the list of notifications</p>
-    </div>
-  );
+  <div className="notifications">
+    <p>Here is the list of notifications</p>
+  </div>
+  )
 }
-
-export default Notifications;
