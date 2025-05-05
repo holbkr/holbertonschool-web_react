9576cce4c829cee979aa2fcabbd2960b94066e0b
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-05 14:04:48 +0200

    fix file and rename file

--- react_component/task_3/dashboard/src/BodySection/BodySection.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySection.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './BodySection.css';
 
-const BodySection = ({ title, children }) => {
-  return (
-    <div className="bodySection">
-      <h2>{title}</h2>
-      {children}
-    </div>
-  );
-};
+class BodySection extends Component {
+
+	render() {
+		const { title, children } = this.props;
+
+		return (
+				<div className="bodySection">
+					<h2>{title}</h2>
+					{children}
+				</div>
+		);
+	}
+}
 
 BodySection.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node,
+	title: PropTypes.string,
+	children: PropTypes.node,
 };
 
 export default BodySection;
