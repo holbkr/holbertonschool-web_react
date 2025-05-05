851978becd8546f1191c3397bc1eb6457a22edff
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-05 13:50:53 +0200

    correction  BodySectionWithMarginBottom

--- react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
+++ react_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import BodySection from './BodySection';
-import PropTypes from 'prop-types';
 import './BodySectionWithMarginBottom.css';
 
-const BodySectionWithMarginBottom = ({ title, children }) => {
-	return (
-	  <div className="bodySectionWithMargin">
-		<BodySection title={title}>
-		  {children}
-		</BodySection>
-	  </div>
-	);
-  };
+function BodySectionWithMarginBottom({ title, children }) {
+  return (
+    <div className="bodySectionWithMargin">
+      <BodySection title={title}>
+        {children}
+      </BodySection>
+    </div>
+  );
+}
 
 export default BodySectionWithMarginBottom;
-BodySectionWithMarginBottom.propTypes = {
-	title: PropTypes.string.isRequired,
-	children: PropTypes.node,
-};
-BodySectionWithMarginBottom.defaultProps = {
-	children: null,
-};
-
