72cbce13df242648e5e0b0058b356aad5897471e
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-08 20:31:20 +0200

    fix code

--- react_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-
-const rowStyle = { backgroundColor: "#f5f5f5ab" };
-const headerStyle = { backgroundColor: "#deb5b545" };
+import { StyleSheet, css } from "aphrodite";
 
 export default function CourseListRow({ isHeader = false, textFirstCell = "", textSecondCell = null }) {
+  const rowClass = isHeader ? css(styles.headerRow) : css(styles.row);
+
   return (
-    <tr style={isHeader ? headerStyle : rowStyle}>
+    <tr className={rowClass}>
       {isHeader ? (
         textSecondCell === null ? (
-          <th colSpan="2">{textFirstCell}</th>
+          <th colSpan="2" className={css(styles.headerCell)}>{textFirstCell}</th>
         ) : (
           <>
-            <th style={{ width: "70%" }}>{textFirstCell}</th>
-            <th>{textSecondCell}</th>
+            <th className={css(styles.headerCell, styles.thLeft)}>{textFirstCell}</th>
+            <th className={css(styles.headerCell)}>{textSecondCell}</th>
           </>
         )
       ) : (
@@ -24,3 +24,20 @@ export default function CourseListRow({ isHeader = false, textFirstCell = "", te
     </tr>
   );
 }
+
+const styles = StyleSheet.create({
+  row: {
+    backgroundColor: "rgba(245, 245, 245, 0.67)"
+  },
+  headerRow: {
+    backgroundColor: "rgba(222, 181, 181, 0.27)"
+  },
+  headerCell: {
+    fontWeight: "bold",
+    textAlign: "left",
+    borderBottom: "1px solid #ccc"
+  },
+  thLeft: {
+    width: "70%"
+  }
+});
