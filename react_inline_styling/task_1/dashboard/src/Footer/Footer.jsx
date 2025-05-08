86b7e2c85e607f5a0623fbb7cabd1101100d08f7
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-08 20:08:25 +0200

    remove file css with footer

--- react_inline_styling/task_1/dashboard/src/Footer/Footer.jsx
+++ react_inline_styling/task_1/dashboard/src/Footer/Footer.jsx
@@ -1,10 +1,23 @@
-import './Footer.css';
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import { getCurrentYear, getFooterCopy } from '../utils/utils';
 
 export default function Footer() {
-    return (
-    <div className="App-footer">
-        <p>Copyright { getCurrentYear() } - { getFooterCopy(true) }</p>
+  return (
+    <div className={css(styles.footer)}>
+      <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
     </div>
-    )
+  );
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    textAlign: 'center',
+    fontStyle: 'italic',
+    borderTop: '3px solid #e11d3f',
+    padding: '1rem 0',
+  },
+});
