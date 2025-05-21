24dd2f89e51e8d789686dc516f2210107626d06f
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-21 09:32:11 +0200

    fix code

---
+++ react_state/task_2/dashboard/src/App/AppContent.js
@@ -0,0 +1,17 @@
+// context.js
+import React from 'react';
+
+export const defaultUser = {
+  email: '',
+  password: '',
+  isLoggedIn: false,
+};
+
+export const defaultLogOut = () => {};
+
+const AppContext = React.createContext({
+  user: defaultUser,
+  logOut: defaultLogOut,
+});
+
+export default AppContext;
