1fe667a41b78003aba321b01da0835fbdb6fe4a4
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-27 10:43:13 +0200

    update footer

--- react_hooks/task_0/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_0/dashboard/src/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import { getCurrentYear, getFooterCopy } from '../utils/utils';
 import AppContext from '../Context/context';
