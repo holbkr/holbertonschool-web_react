08ee8fc300af787e4e3b82baa36611f9a5992e4f
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-05-27 10:37:51 +0200

    remove React import from Header.jsx as required by project constraints

--- react_hooks/task_0/dashboard/src/Header/Header.jsx
+++ react_hooks/task_0/dashboard/src/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { StyleSheet, css } from 'aphrodite';
 import holbertonLogo from '../assets/holberton-logo.jpg';
 import AppContext from '../Context/context';
