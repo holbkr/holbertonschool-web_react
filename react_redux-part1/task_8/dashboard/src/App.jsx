c9b6982f15701181e3037dd8347f52296c828172
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-06-11 22:50:26 +0200

    refactor: Simplify App component by removing unused imports and restructuring for clarity

--- react_redux-part1/task_8/dashboard/src/App.jsx
+++ react_redux-part1/task_8/dashboard/src/App.jsx
@@ -1,48 +1,14 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchNotifications } from './redux/notificationSlice';
-import { fetchCourses } from './redux/courseSlice';
-import Notifications from './components/Notifications/Notifications';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+// src/App.jsx
+import { useSelector } from 'react-redux';
 import Login from './pages/Login/Login';
 import CourseList from './pages/CourseList/CourseList';
-import BodySectionWithMarginBottom from './components/BodySectionWithMarginBottom/BodySectionWithMarginBottom';
-import BodySection from './components/BodySection/BodySection';
 
 export default function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchNotifications());
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      dispatch(fetchCourses());
-    }
-  }, [isLoggedIn, dispatch]);
 
   return (
-    <>
-      <Notifications />
-      <Header />
-      <main>
-        {!isLoggedIn ? (
-          <BodySectionWithMarginBottom title="Log in to continue">
-            <Login />
-          </BodySectionWithMarginBottom>
-        ) : (
-          <BodySectionWithMarginBottom title="Course list">
-            <CourseList />
-          </BodySectionWithMarginBottom>
-        )}
-        <BodySection title="News from the School">
-          <p>Holberton School news goes here</p>
-        </BodySection>
-      </main>
-      <Footer />
-    </>
+    <div>
+      {isLoggedIn ? <CourseList /> : <Login />}
+    </div>
   );
 }
