59b7acd1d5f365ee97f7b4eeae26a15db1861c8d
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-06-11 22:55:06 +0200

    refactor: Enhance App component structure and functionality by integrating notifications and courses fetching

--- react_redux-part1/task_8/dashboard/src/App.jsx
+++ react_redux-part1/task_8/dashboard/src/App.jsx
@@ -1,14 +1,48 @@
-// src/App.jsx
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchNotifications } from './redux/notificationSlice';
+import { fetchCourses } from './redux/courseSlice';
+import Notifications from './components/Notifications/Notifications';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 import Login from './pages/Login/Login';
 import CourseList from './pages/CourseList/CourseList';
+import BodySectionWithMarginBottom from './components/BodySectionWithMarginBottom/BodySectionWithMarginBottom';
+import BodySection from './components/BodySection/BodySection';
 
 export default function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchNotifications());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(fetchCourses());
+    }
+  }, [isLoggedIn, dispatch]);
 
   return (
-    <div>
-      {isLoggedIn ? <CourseList /> : <Login />}
-    </div>
+    <>
+      <Notifications />
+      <Header />
+      <main>
+        {!isLoggedIn ? (
+          <BodySectionWithMarginBottom title="Log in to continue">
+            <Login />
+          </BodySectionWithMarginBottom>
+        ) : (
+          <BodySectionWithMarginBottom title="Course list">
+            <CourseList />
+          </BodySectionWithMarginBottom>
+        )}
+        <BodySection title="News from the School">
+          <p>Holberton School news goes here</p>
+        </BodySection>
+      </main>
+      <Footer />
+    </>
   );
 }
